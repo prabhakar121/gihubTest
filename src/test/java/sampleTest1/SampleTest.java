ad15feedcc2951039214633efe7b3279e0601341
prabhakar121 <prabhakar121@gmail.com>
2022-01-26 15:22:21 +0530

    GoogleScreenShot

--- src/test/java/sampleTest1/SampleTest.java
+++ src/test/java/sampleTest1/SampleTest.java
@@ -3,36 +3,28 @@ package sampleTest1;
 import java.io.File;
 import java.io.IOException;
 
-import org.junit.Test;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.io.FileHandler;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SampleTest {
-	WebDriver wd = null;
-
 	@Test
-	public void loadGoogle() {
+	public void googleTest() throws IOException{
 		WebDriverManager.chromedriver().setup();
-		wd = new ChromeDriver();
-
-		wd.manage().window().maximize();
+		WebDriver wd = new ChromeDriver();
 		wd.get("https://www.google.co.in/");
-		wd.findElement(By.name("q")).sendKeys("chennai");
-		wd.findElement(By.name("q")).submit();
+		wd.findElement(By.name("q")).sendKeys("dubai");
+		wd.findElement(By.name("q")).sendKeys(Keys.ENTER );
+		
+	File sh=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+	FileUtils.copyFile(sh, new File("screenshotsTake/google.png"));
 	}
-
-	@AfterMethod
-	public void tearDown() throws IOException {
-		File scr = ((TakesScreenshot) wd).getScreenshotAs(OutputType.FILE);
-		FileHandler.copy(scr, new File("\\Users\\Admin\\workspace\\C2_WebdriverTest\\ScreenShot\\google.jpeg"));
-
-	}
-
+	
 }
